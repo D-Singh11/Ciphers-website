0f06b9bb3deab665ebde79b3f1ceb17901c7a604
ghost <sssscorejava@gmail.com>
2018-02-10 03:15:54 +0000

    Modified Javascript file link in cipher2.html. Added input box and submit button to cipher.html for user input. Created function for second cipher in javascript file and linked it to cipher.html file. Tested functionality of website and all ok.

--- Cipher_Website/JavaScript/cipher1.js
+++ Cipher_Website/JavaScript/cipher1.js
@@ -10,7 +10,7 @@ function ROT13()
     for(i=0;i<user_Input.length; i++)           // for loop to iterate through user input
     {
         
-    var unicode = user_Input.charCodeAt(i);             // variable to get unicode of current character in loop
+        var unicode = user_Input.charCodeAt(i);             // variable to get unicode of current character in loop
         if(unicode >=65 && unicode <=77)                    // condition to applt rot 13
             {
                 cipheredText +=  String.fromCharCode(unicode+13);   //converted letter gets concatenated & stored in variable
@@ -30,4 +30,56 @@ function ROT13()
     input_Element.value = "";
     var show_Output = document.getElementById("result");
     show_Output.innerHTML= cipheredText;
+}
+
+
+
+// CIpher 2 
+
+var previous = 0;
+function Cipher2()
+{
+    
+    var input2 = document.getElementById("input2").value.toUpperCase();    // get user input from webpage
+   
+    var keySum = 0;                                         // cipher key number to be used
+    
+    
+    
+    for(i=0; i<input2.length; i++)
+        {
+            keySum = keySum + input2.charCodeAt(i);         // working out key number by iterating through all chracters
+        }
+   
+   
+    var key = keySum;                               // key value used to aapplu cipher algoritham
+    
+        if(keySum == previous)
+         {
+                keySum += 1;
+                key = keySum;
+         }
+    
+    previous = keySum;
+    keySum = keySum % 13;                       // further operation on key value to be used for encryption
+    
+    
+    var cipherText = "";
+    for(i=0; i<input2.length; i++)
+       {
+            var unico = input2.charCodeAt(i);       // looping thoriugh every single input charcter to convert it into ciphered text
+            
+            if(unico >=65 && unico <=90)              // only convert ciphered text if its UNICODE in specified range
+             {
+                 cipherText = cipherText + String.fromCharCode(unico + keySum);             // finally encrypting input to cipher text
+             }
+        
+           
+       }
+    
+    
+    cipherText = cipherText.concat(key);                   // this is to give hint to decrypter to decode text
+    
+    var output2 = document.getElementById("result2");
+    output2.innerHTML = cipherText.toString();              // display result
 }
